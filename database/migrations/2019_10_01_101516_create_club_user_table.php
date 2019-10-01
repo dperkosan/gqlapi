4a93b59ea7b5cd7215885ac4ce5e1023c159fb87
Damir Perkosan <damir.perkosan@diwanee.com>
2019-10-01 12:25:29 +0200

    relationship between users and clubs

---
+++ database/migrations/2019_10_01_101516_create_club_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClubUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('club_user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('club_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('club_user');
+    }
+}
